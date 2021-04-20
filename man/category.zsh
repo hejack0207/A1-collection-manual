e142d2aefc4b9f465375427dca2b52df3652974a
hejack <hejack0207@sina.com>
2021-04-20 08:41:20 +0800

    fix

---
+++ man/category.zsh
@@ -0,0 +1,8 @@
+pattern=${1:?pattern not set}
+category=${2}
+subdirs=(groff md txt)
+
+for subdir in $subdirs; do
+	test -d $subdir/$category || mkdir $subdir/$category
+	eval mv $subdir/*${pattern}* $subdir/$category/
+done
