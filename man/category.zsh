f6191cfc0b76982827528f7c79106b573d3bf377
hejack <hejack0207@sina.com>
2021-04-20 09:07:21 +0800

    fix

--- man/category.zsh
+++ man/category.zsh
@@ -1,8 +1,11 @@
+#!/usr/bin/zsh
+setopt -o extendedglob
+
 pattern=${1:?pattern not set}
 category=${2}
 subdirs=(groff md txt)
 
 for subdir in $subdirs; do
 	test -d $subdir/$category || mkdir $subdir/$category
-	eval mv $subdir/*${pattern}* $subdir/$category/
+	eval mv $subdir/${pattern}*(.) $subdir/$category/
 done
