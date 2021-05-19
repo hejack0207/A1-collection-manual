f7784e6f6866fb5cc93ab69e7be7f005d329e2c7
hejack <hejack0207@sina.com>
2021-05-20 04:27:55 +0800

    fix

---
+++ rfc/mig.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+
+rfcnums=${1:-$(jq -r '.[]|.number' rfcs.json)}
+for n in $rfcnums; do
+	cate=$(jq -r ".[]|select( .number == $n )|.category" rfcs.json)
+	title=$(jq -r ".[]|select( .number == $n )|.title" rfcs.json)
+	cp ~codes/docs/rfc/rfc$n.txt ~rfc/$cate/rfc$n-$title.txt
+done
+
