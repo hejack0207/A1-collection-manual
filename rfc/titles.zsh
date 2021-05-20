e7f278f881bc9f12b6c4f36437f40de577daaa0e
hejack <hejack0207@sina.com>
2021-05-21 05:58:22 +0800

    fix

--- rfc/titles.zsh
+++ rfc/titles.zsh
@@ -1,2 +1,2 @@
 #!/usr/bin/env -S zsh
-awk -e '/^[[:digit:]]{4}/{rfc=$0;output="1"}' -e '/^[[:space:]]{4}/{rfc=rfc $0}' -e '/^$/ && output=="1"{gsub("[[:cntrl:]]","",rfc);gsub("[[:space:]]{2,}"," ",rfc);print rfc}' rfc-index.txt
+awk -e '/^[[:digit:]]{4}/{rfc=$0;output="1"}' -e '/^[[:space:]]{4}/{rfc=rfc $0}' -e '/^\r$/ && output=="1"{gsub("[[:cntrl:]]","",rfc);gsub("[[:space:]]{2,}"," ",rfc);print rfc}' rfc-index.txt
