a1efec0a46683f2254521ada080a714c25de1638
hejack <hejack0207@sina.com>
2021-05-22 09:14:16 +0800

    fix

--- rfc/rfcnums.zsh
+++ rfc/rfcnums.zsh
@@ -1,2 +1,2 @@
 #!/usr/bin/env -S zsh
-find . -regextype gnu-awk -regex '.*/rfc[[:digit:]]{4}.*' | sed -nre 's/.*rfc([[:digit:]]{4}).*/\1/p'
+find . -regextype gnu-awk -regex '.*/rfc[[:digit:]]{4}.*' | sed -nre 's#\./(.*)/rfc([[:digit:]]{4}).*#\2\x00\1#p'
