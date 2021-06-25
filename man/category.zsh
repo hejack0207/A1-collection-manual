dce392043c5d2c73c9d944853cbe675bbe2d3172
hejack <hejack0207@sina.com>
2021-06-25 20:16:47 +0800

    fix

--- man/category.zsh
+++ man/category.zsh
@@ -7,5 +7,5 @@ subdirs=(groff md txt)
 
 for subdir in $subdirs; do
 	test -d $subdir/$category || mkdir $subdir/$category
-	eval mv $subdir/${pattern}*(.) $subdir/$category/
+	eval mv $subdir/*${pattern}*(.) $subdir/$category/
 done
