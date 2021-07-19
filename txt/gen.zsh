de7911f5f2d3c1bceb4c727124b4382ba52d46d3
hejack <hejack0207@sina.com>
2021-07-19 20:17:48 +0800

    fix

--- txt/gen.zsh
+++ txt/gen.zsh
@@ -6,15 +6,17 @@ if [ $# -lt 1 ]; then
 fi
 
 txtfile=${1:-../man/txt/bash.1.txt}
-name=${$(basename $txtfile)%%.*}
+name=${$(basename $txtfile)%.?.*}
 
-rm -rf $name
+if test -d $name; then
+	exit 0;
+fi
 mkdir $name
 
 csplit $txtfile -f "$name/$name-" -b "%02d.txt" '/^[[:alpha:]][[:alpha:][:space:]]\+$/' '{*}'
 
 for f in $name/$name-*.txt; do
-	i=${${f%%.txt}##$name/$name-}
+	i=${${f%.?.txt}##$name/$name-}
 	mv $name/$name-$i.txt "$name/$i-$(head -n1 $f | tr 'A-Z ' 'a-z-').txt"
 done
 rm $name/00-*
