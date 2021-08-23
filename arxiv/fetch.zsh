742563da13fb0ff309f56109f0fee46476e8d90c
hejack <hejack0207@sina.com>
2021-08-23 17:19:30 +0800

    fix

--- arxiv/fetch.zsh
+++ arxiv/fetch.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S zsh -il
 
-cat arxiv.tsv | while read id title category; do
+cat arxiv.tsv | tail -n +2 | while read id title category; do
 	echo $id $title $category
 	# arxiv-fetch
 done
