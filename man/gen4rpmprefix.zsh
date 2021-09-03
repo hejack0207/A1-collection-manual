12e952b8d4deb4839150678856b56149b8b58098
hejack <hejack0207@sina.com>
2021-09-03 14:51:41 +0800

    fix

---
+++ man/gen4rpmprefix.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/env -S zsh -il
+# set -x
+
+usage() {
+    echo "Usage: $0 [-s <section num>] [-c category] -D -h" 1>&2;
+    exit 1;
+}
+
+sect=""
+category=""
+debug=0
+while getopts "p:s:Dh" o; do
+    case "${o}" in
+        s)
+            sect=${OPTARG}
+            ;;
+        c)
+            category=${OPTARG}
+            ;;
+        D)
+            debug=1
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+for p in $(rpm -qa G $1); do
+	if rpm -ql $p | grep -q /usr/share/man; then
+		print $p;
+	fi
+done | awk -F- '{print $1"-"$2}'
