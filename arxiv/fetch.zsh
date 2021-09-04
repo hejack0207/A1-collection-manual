0a41621ac7e36b59876c97b6643a0ff335cb1890
hejack <hejack0207@sina.com>
2021-09-04 13:48:14 +0800

    fix

--- arxiv/fetch.zsh
+++ arxiv/fetch.zsh
@@ -1,6 +1,12 @@
 #!/usr/bin/env -S zsh -il
 
-cat arxiv.tsv | tail -n +2 | while read id title category; do
-	echo $id $title $category
-	arxiv-sfetch.py $id
+cat arxiv.tsv | tail -n +2 | while read id subcategory; do
+	echo $id $category
+	if test -f arxiv-archieved.tsv && grep -q $id arxiv-archieved.tsv; then
+		echo skipped
+		continue
+	fi
+	if arxiv-query.py -j $id | jyt -f json -t yaml | grep --color=never -v '^---$' >>arxiv.yml; then
+		print $id >> arxiv-archieved.tsv
+	fi
 done
