62d6cb99483048d4ef818598992650eb1f948d54
hejack <hejack0207@sina.com>
2022-05-29 16:06:05 +0800

    fix

--- man/gen4rpm.zsh
+++ man/gen4rpm.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env -S zsh -il
+# vim: sts=-1 sw=4
 # set -x
 
 usage() {
@@ -10,7 +11,7 @@ pkg=""
 sect=""
 category=""
 debug=0
-while getopts "p:s:Dh" o; do
+while getopts "p:s:c:Dh" o; do
     case "${o}" in
         p)
             pkg=${OPTARG}
@@ -36,29 +37,28 @@ shift $((OPTIND-1))
 echo "listing manuals for package $pkg"
 grepv=()
 if [[ "$pkg" = "man-pages" && "$sect" = "7" ]]; then
-	grepv=(\| grep -v -e utf-8 -e iso_ -e urn -e url -e latin)
+    grepv=(\| grep -v -e utf-8 -e iso_ -e urn -e url -e latin)
 fi
 if [[ "$pkg" = "man-pages" && "$sect" = "2" ]]; then
-	grepv=(\| grep -v -e _Exit.2)
+    grepv=(\| grep -v -e _Exit.2)
 fi
 pkgversion=$(rpm -qi $pkg | grep Version | sed -re 's/(.*:\s+)//g')
 mans=()
 for p in $(rpm -ql $pkg | grep -E --color=never -e "/man/man([^/]*|$sect)/" ${grepv[@]});
 do
-	(( debug )) && echo $p
-	mpage=($(echo $p | sed -re 's#.*/(.*\.[[:digit:]].*)\.gz#\1#g'))
-	mans+=($mpage)
-	(( debug )) && echo $mpage
+    (( debug )) && echo $p
+    mpage=($(echo $p | sed -re 's#.*/(.*\.[[:digit:]].*)\.gz#\1#g'))
+    mans+=($mpage)
+    (( debug )) && echo $mpage
 done
 echo "man pages: ${mans[@]}"
 set -- ${mans[@]}
 
-test -z $category || category=$category/
-test -d groff/$category$pkg$sect || mkdir -p groff/$category$pkg$sect
-test -d txt/$category$pkg$sect || mkdir -p txt/$category$pkg$sect
-test -d md/$category$pkg$sect || mkdir -p md/$category$pkg$sect
+test -d groff/$category/$pkg$sect || mkdir -p groff/$category/$pkg$sect
+test -d txt/$category/$pkg$sect || mkdir -p txt/$category/$pkg$sect
+test -d md/$category/$pkg$sect || mkdir -p md/$category/$pkg$sect
 for manname; do
-	zcat $(man -w $manname) >groff/$category$pkg$sect/$manname
-	man_dump $manname >txt/$category$pkg$sect/$manname.txt
-	MANWIDTH=40 zcat $(man -w $manname) | awk '/^.TH.*/ || p { p = 1; print }' | man-to-md.pl >md/$category$pkg$sect/$manname.md
+    zcat $(man -w $manname) >groff/$category/$pkg$sect/$manname
+    man_dump $manname >txt/$category/$pkg$sect/$manname.txt
+    MANWIDTH=40 zcat $(man -w $manname) | awk '/^.TH.*/ || p { p = 1; print }' | man-to-md.pl >md/$category/$pkg$sect/$manname.md
 done
