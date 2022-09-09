fcded07374950c5f71f72470882a190c22e81ed5
hejack <hejack0207@sina.com>
2022-09-09 21:39:44 +0800

    fix

--- txt/rfc/migt.zsh
+++ txt/rfc/migt.zsh
@@ -1,16 +1,14 @@
 #!/usr/bin/env -S zsh -i
 
-./gentsv.zsh
-
 cat rfcs.tsv | while read n cate title; do
 	ntitle=$(echo -n "$title" | tr '/' '-')
-	test -d ~rfc/$cate || mkdir -p ~rfc/$cate
-	if ! test -f ~rfc/$cate/rfc$n-$ntitle.txt; then
+	test -d ~rfc/rfcs/$cate || mkdir -p ~rfc/rfcs/$cate
+	if ! test -f ~rfc/rfcs/$cate/rfc$n-$ntitle.txt; then
 		sn=$(print -f "%d" $n)
-		rm ~rfc/$cate/rfc$n-*.txt
-		cp ~codes/docs/rfc/rfc$sn.txt ~rfc/$cate/"rfc$n-$ntitle.txt"
+		rm ~rfc/rfcs/$cate/rfc$n-*.txt
+		cp ~codes/docs/rfc/rfc$sn.txt ~rfc/rfcs/$cate/"rfc$n-$ntitle.txt"
 	else
-		echo ~rfc/$cate/rfc$n-$ntitle.txt already exists!
+		echo ~rfc/rfcs/$cate/rfc$n-$ntitle.txt already exists!
 	fi
 done
 
