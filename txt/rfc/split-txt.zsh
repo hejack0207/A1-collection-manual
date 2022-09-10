606d9a9af3cf0e2771b7c880d7a96fd719b5e9bd
hejack <hejack0207@sina.com>
2022-09-10 16:42:42 +0800

    fix

--- txt/rfc/split-txt.zsh
+++ txt/rfc/split-txt.zsh
@@ -4,27 +4,25 @@
 
 awkscript="$(pwd)/split-txt.awk"
 
-if ! find rfcs -regextype awk -regex '.*/rfc[[:digit:]]+.*' -type d &>/dev/null; then
-    while read f; do
-	if [[ "$f" != *rfc0826* ]]; then
-	    dir=$(dirname "$f")
-	    subdir=$(basename "$f")
-	    subdir=${subdir%*.txt}
-	    test -e "$dir/$subdir" || mkdir "$dir/$subdir"
+while read f; do
+    if [[ "$f" != *rfc0826* ]]; then
+	dir=$(dirname "$f")
+	subdir=$(basename "$f")
+	subdir=${subdir%*.txt}
+	if ! test -e "$dir/$subdir"; then
+	    mkdir "$dir/$subdir"
 	    cp "$f" "$dir/$subdir"
 	fi
-    done <<<$(find "rfcs" -maxdepth 2 -name '*.txt')
-fi
+    fi
+done <<<$(find "rfcs" -maxdepth 2 -name '*.txt')
 
-if ! find rfcs | grep -q Abstract; then
-    while read f; do
-	if [[ "$f" != *rfc0826* ]]; then
-	    dir=$(dirname "$f")
-	    pushd "$dir"
-	    fn=$(basename ${f})
-	    awk -f "$awkscript" "$fn"
-	    rm "$f"
-	    popd
-	fi
-    done <<<$(find rfcs -mindepth 3 -type f -regextype awk -regex '.*/[^[:digit:]]+[^/]*\.txt')
-fi
+while read f; do
+    if [[ "$f" != *rfc0826* ]]; then
+	dir=$(dirname "$f")
+	pushd "$dir"
+	fn=$(basename ${f})
+	awk -f "$awkscript" "$fn"
+	rm "$f"
+	popd
+    fi
+done <<<$(find rfcs -mindepth 3 -type f -regextype awk -regex '.*/[^[:digit:]]+[^/]*\.txt')
