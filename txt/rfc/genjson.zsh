7b624c45dcb499d4b58c950215b9c07283d81319
hejack <hejack0207@sina.com>
2022-09-10 12:32:57 +0800

    fix

--- txt/rfc/genjson.zsh
+++ txt/rfc/genjson.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 
 yq '.' rfcs.yml | jq --slurpfile nums =(cat rfcnums.tsv | while read n _; do echo $n; done) '[.[] | select([.number] | inside($nums))]' >rfcs.json
-cat rfcs.json | jq --slurpfile cates =(cat rfcnums.tsv | while read n c; do echo '{ "number": '$n', "cate": "'$c'"}'; done) '[.[] |= (.number as $n | .category=($cates | .[] | select(.number == $n)|.cate))]' | sponge rfcs.json
+cat rfcs.json | jq --slurpfile cates =(cat rfcnums.tsv | while read n c; do echo '{ "number": '$n', "cate": "'$c'"}'; done) '.[] |= (.number as $n | .category=($cates | .[] | select(.number == $n)|.cate))' | sponge rfcs.json
