557876f42cfd0001f0536091ae0c10858c48e011
hejack <hejack0207@sina.com>
2022-09-10 09:49:07 +0800

    fix

--- txt/rfc/split-txt.zsh
+++ txt/rfc/split-txt.zsh
@@ -1,17 +1,27 @@
 # vim: sts=-1 sw=4 fdm=marker
 
 awkscript="$(pwd)/split-txt.awk"
-while read f; do
-    if [[ "$f" != *rfc0826* ]]; then
-	dir=$(dirname "$f")
-	subdir=$(basename "$f")
-	subdir=${subdir%*.txt}
-	mkdir "$dir/$subdir"
-	pushd "$dir/$subdir"
-	mv "$f" .
-	awk -f "$awkscript" $(basename "$f")
-	popd
-	# echo ----------
-	# awk -f "$awkscript" "$f"
-    fi
-done <<<$(find "rfcs" -name '*.txt')
+
+if ! find rcfs -regextype awk -regex '.*/rfc[[:digit:]]+.*' -type d &>/dev/null; then
+    while read f; do
+	if [[ "$f" != *rfc0826* ]]; then
+	    dir=$(dirname "$f")
+	    subdir=$(basename "$f")
+	    subdir=${subdir%*.txt}
+	    mkdir "$dir/$subdir"
+	    mv "$f" "$dir/$subdir"
+	fi
+    done <<<$(find "rfcs" -name '*.txt')
+fi
+
+if ! find | grep -q Abstract; then
+    while read f; do
+	if [[ "$f" != *rfc0826* ]]; then
+	    dir=$(dirname "$f")
+	    pushd "$dir"
+	    awk -f "$awkscript" $(basename "$f")
+	    popd
+	fi
+    done <<<$(find "rfcs" -name '*.txt')
+fi
+
