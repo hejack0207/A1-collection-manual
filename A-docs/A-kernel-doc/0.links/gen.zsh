c2464a506b152b1c2866373046a5c2d307c1ac4f
hejack <hejack0207@sina.com>
2022-09-16 23:44:44 +0800

    fix

--- A-docs/A-kernel-doc/0.links/gen.zsh
+++ A-docs/A-kernel-doc/0.links/gen.zsh
@@ -2,11 +2,12 @@
 # vim: sts=-1 sw=4 foldmethod=marker
 
 local -A opts
-zparseopts -K -D -M -E -A opts -help h d
+zparseopts -K -D -M -E -A opts -help h d x:
 
 usage(){
     print "Usage: gen.zsh [options] keyword"
     print "\t-d\tdry run mode"
+    print "\t-x pattern\texclude pattern"
     print "\t-h\tshow this help"
 }
 
@@ -18,8 +19,16 @@ fi
 key=${1:?key not specified}
 
 if [[ -n ${opts[(i)-d]} ]]; then
-    find ../.. -path "*$key*" -type f | grep -v links/$key | while read p; do echo "$p"; done
+    if [[ -n ${opts[-x]} ]]; then
+	find ../.. -path "*$key*" -type f | grep -v links/$key | grep -v "${opts[-x]}" | while read p; do echo "$p"; done
+    else
+	find ../.. -path "*$key*" -type f | grep -v links/$key | while read p; do echo "$p"; done
+    fi
 else
-    find ../.. -path "*$key*" -type f | grep -v links/$key | while read p; do ln -s $p ${${p#../../}//\//__}; done
+    if [[ -n ${opts[-x]} ]]; then
+	find ../.. -path "*$key*" -type f | grep -v links/$key | grep -v "${opts[-x]}" | while read p; do ln -s $p ${${p#../../}//\//__}; done
+    else
+	find ../.. -path "*$key*" -type f | grep -v links/$key | while read p; do ln -s $p ${${p#../../}//\//__}; done
+    fi
 fi
 
