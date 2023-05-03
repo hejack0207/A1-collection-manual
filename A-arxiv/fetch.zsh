539057658b740223018526e97bbd0344d3e27b24
hejack <hejack0207@sina.com>
2023-05-04 04:59:18 +0800

    fix

--- A-arxiv/fetch.zsh
+++ A-arxiv/fetch.zsh
@@ -1,9 +1,24 @@
 #!/usr/bin/env -S zsh -il
 
+id=${1:?id not specified}
+category=${2:?category not specified}
+
+if [[ ! -d latex/$id ]]; then
+	arxiv-sfetch.py --target_dir=latex $id
+else
+	echo "latex/$id already exists"
+fi
+
+
+if ! grep -q "$id" arxiv.tsv; then
+	echo "$id\x0$category" >>arxiv.tsv 
+else
+	echo "$id already exists in arxiv.tsv"
+fi
+
 cat arxiv.tsv | tail -n +2 | while read id subcategory; do
-	echo $id $category
 	if test -f arxiv-archieved.tsv && grep -q $id arxiv-archieved.tsv; then
-		echo skipped
+		echo "$id $category already in arxiv-archieved.tsv"
 		continue
 	fi
 	if arxiv-query.py -j $id | jyt -f json -t yaml | grep --color=never -v '^---$' >>arxiv.yml; then
