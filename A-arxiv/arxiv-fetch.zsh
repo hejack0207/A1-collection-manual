2ff302f6cfb6c3f272645e6dd5852cfcf86886ba
hejack <hejack0207@sina.com>
2024-06-04 13:47:03 +0800

    fix

--- A-arxiv/arxiv-fetch.zsh
+++ A-arxiv/arxiv-fetch.zsh
@@ -1,27 +1,36 @@
 #!/usr/bin/env -S zsh -il
 
+basedir=$(dirname ${0})
+print -u 2 $basedir
+
 id=${1:?id not specified}
 category=${2:?category not specified}
 
+title=$(arxiv-query.py -j $id | jq -r '.[0].title')
+if [[ -z "$title" ]] then
+	print -u 2 "title is empty, quit"
+	return 1
+fi
+
 if [[ ! -d latex/$id ]]; then
-	arxiv-fetch.py --target_dir=latex $id
+	arxiv-fetch.py --target_dir=$basedir/latex/$title $id
 else
 	echo "latex/$id already exists"
 fi
 
 
-if ! grep -q "$id" arxiv.tsv; then
-	echo "$id\x0$category" >>arxiv.tsv 
+if ! grep -q "$id" $basedir/arxiv.tsv; then
+	echo "$id\x0$category" >>$basedir/arxiv.tsv
 else
 	echo "$id already exists in arxiv.tsv"
 fi
 
-cat arxiv.tsv | tail -n +2 | while read id subcategory; do
-	if test -f arxiv-archieved.tsv && grep -q $id arxiv-archieved.tsv; then
+cat $basedir/arxiv.tsv | tail -n +2 | while read id subcategory; do
+	if test -f $basedir/arxiv-archieved.tsv && grep -q $id $basedir/arxiv-archieved.tsv; then
 		echo "$id $category already in arxiv-archieved.tsv"
 		continue
 	fi
-	if arxiv-query.py -j $id | jyt -f json -t yaml | grep --color=never -v '^---$' >>arxiv.yml; then
-		print $id >> arxiv-archieved.tsv
+	if arxiv-query.py -j $id | jyt -f json -t yaml | grep --color=never -v '^---$' >>$basedir/arxiv.yml; then
+		print $id >> $basedir/arxiv-archieved.tsv
 	fi
 done
